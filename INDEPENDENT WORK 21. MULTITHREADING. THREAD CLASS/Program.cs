33ab972ac65f847c6cedf72e991590a1fb6ac53b
Tolmachev DY <Tolmachev DY@DESKTOP-5RJHKAS>
2022-11-28 14:44:31 +0300

    Добавьте файлы проекта.

---
+++ INDEPENDENT WORK 21. MULTITHREADING. THREAD CLASS/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace INDEPENDENT_WORK_21.MULTITHREADING.THREAD_CLASS
+{
+    class Program
+    {
+        const int n = 2;
+        const int k = 3;
+        static int[,] path = new int[n, k] { { 0, 1, 2 }, { 3, 4, 5 } };
+
+        static void Main(string[] args)
+        {
+            ThreadStart threadStart = new ThreadStart(Gardner1);
+            Thread thread = new Thread(threadStart);
+            thread.Start();
+
+            Gardner2();
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    Console.Write("{0} ", path[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        static void Gardner1()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    if (path[i, j] >= 0)
+                    {
+                        int delay = path[i, j];
+                        path[i, j] = -1;
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+        static void Gardner2()
+        {
+            for (int j = k - 1; j < k && j >= 0; j--)
+            {
+                for (int i = n - 1; i < n && i >= 0; i--)
+                {
+                    if (path[i, j] >= 0)
+                    {
+                        int delay = path[i, j];
+                        path[i, j] = -2;
+                        Thread.Sleep(delay);
+                    }
+                }
+            }
+        }
+
+    }
+}
